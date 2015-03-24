14a2c7cb907a5b3dce56604da54bc1eee249d766
parpar-d <parisadolatabadi@yahoo.com>
2015-03-24 14:31:53 +0430

    two step verification

--- test/start.php
+++ test/start.php
@@ -12,7 +12,7 @@ function test_init()
 	//elgg_extend_view('page/elements/header', 'test/save');
 	//elgg_register_ajax_view('edit');
     
-	elgg_register_action('test/save', elgg_get_plugins_path() . 'test/usersettings/save.php');
+	elgg_register_action('test/save', elgg_get_plugins_path() . 'test/actions/plugins/usersettings/save.php');
     //$action_base = elgg_get_plugins_path() . 'test/actions';
     //elgg_register_action("test", "$action_base/edit.php");
 }
