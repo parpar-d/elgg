ca2af80f3d48665c38c5260336ad54c57f9e6821
parpar-d <parisadolatabadi@yahoo.com>
2015-04-01 16:24:31 +0430

    two step verification

--- test/start.php
+++ test/start.php
@@ -11,9 +11,7 @@ function test_init()
 	$actionspath = $CONFIG->pluginspath . "test/actions/test";
 	elgg_register_action("test/usersettings/save", "$actionspath/usersettings/save.php");
 	elgg_extend_view('forms/login', 'test/login');
-	//elgg_extend_view('forms/profile/edit', 'test/edit');
-	//elgg_extend_view('page/elements/header', 'test/save');
-	//elgg_register_ajax_view('edit');
+	
     
 	//elgg_register_action('test/save', elgg_get_plugins_path() . 'test/actions/plugins/usersettings/save.php');
     //$action_base = elgg_get_plugins_path() . 'test/actions';
